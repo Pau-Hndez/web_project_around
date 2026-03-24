a26f93ea8e1fd70656f7cee81ec43b522203487f
Paula Hernandez <phernandez.musica@gmail.com>
2026-03-23 19:26:41 -0500

    added POO

--- components/Card.js
+++ components/Card.js
@@ -1,39 +1,39 @@
-import { handlePhotoPopup } from "./Utils.js";
 export class Card {
-  constructor(data, cardTemplate) {
+  constructor(data, cardTemplate, handleCardClick) {
     this._name = data.name;
     this._link = data.link;
     this._cardTemplate = cardTemplate;
+    this._handleCardClick = handleCardClick;
   }
+
   _getTemplate() {
-    const cardElement = document
+    return document
       .querySelector(this._cardTemplate)
       .content.querySelector(".card")
       .cloneNode(true);
-    return cardElement;
   }
+
   _handleLikeClick() {
     this._likeButton.classList.toggle("card__like-button_active");
   }
-  _handleDeleteClick(evt) {
-    const button = evt.target;
-    const card = button.closest(".card");
-    card.remove();
+
+  _handleDeleteClick() {
+    this._element.remove();
+    this._element = null;
   }
+
   _setEventListeners() {
-    this._likeButton.addEventListener("click", () => {
-      this._handleLikeClick();
-    });
-    this._deleteButton.addEventListener("click", () => {
-      this._handleDeleteClick();
-    });
-    this._image.addEventListener("click", (evt) => {
-      this.handlePhotoPopup(evt);
+    this._likeButton.addEventListener("click", () => this._handleLikeClick());
+    this._deleteButton.addEventListener("click", () =>
+      this._handleDeleteClick(),
+    );
+    this._image.addEventListener("click", () => {
+      this._handleCardClick(this._name, this._link);
     });
   }
+
   generateCard() {
     this._element = this._getTemplate();
-
     this._image = this._element.querySelector(".card__picture");
     this._likeButton = this._element.querySelector(".card__like-button");
     this._deleteButton = this._element.querySelector(".card__delete-button");
@@ -44,9 +44,6 @@ export class Card {
     this._image.alt = this._name;
 
     this._setEventListeners();
-
     return this._element;
   }
 }
-
-export default Card;
---
+++ page/index.js
@@ -0,0 +1,118 @@
+import { Card } from "../components/Card.js";
+import { FormValidator } from "../components/FormValidator.js";
+import Section from "../components/Section.js";
+import PopupWithImage from "../components/PopupWithImage.js";
+import PopupWithForm from "../components/PopupWithForm.js";
+import UserInfo from "../components/UserInfo.js";
+
+const initialCards = [
+  {
+    name: "Valle de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg",
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg",
+  },
+  {
+    name: "Montañas Calvas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg",
+  },
+  {
+    name: "Parque Nacional de la Vanoise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",
+  },
+];
+
+const validationConfig = {
+  inputSelector: ".modal__input",
+  submitButtonSelector: ".modal__save",
+  inactiveButtonClass: "modal__save_inactive",
+  inputErrorClass: "modal__input:invalid",
+  errorClass: ".modal__input-error",
+};
+
+// --- Instancias ---
+
+// 1. Información de Usuario
+const userInfo = new UserInfo({
+  nameSelector: ".profile__name",
+  aboutSelector: ".profile__about-me",
+});
+
+// 2. Popup de Imagen
+const imagePopup = new PopupWithImage(".photo");
+imagePopup.setEventListeners();
+
+// 3. Función para crear tarjetas (reutilizable)
+const createCard = (data) => {
+  const card = new Card(data, "#card__template", (name, link) => {
+    imagePopup.open(name, link);
+  });
+  return card.generateCard();
+};
+
+// 4. Sección de Tarjetas
+const cardSection = new Section(
+  {
+    items: initialCards,
+    renderer: (item) => {
+      const cardElement = createCard(item);
+      cardSection.addItem(cardElement);
+    },
+  },
+  ".cards__list",
+);
+
+cardSection.renderItems();
+
+// 5. Popup Formulario Perfil
+const profilePopup = new PopupWithForm("#popup-profile", (data) => {
+  userInfo.setUserInfo(data);
+  profilePopup.close();
+});
+profilePopup.setEventListeners();
+
+// 6. Popup Formulario Nuevo Lugar
+const placePopup = new PopupWithForm("#popup-place", (data) => {
+  // data viene con 'title' y 'picture' según los names de los inputs en el HTML
+  const newCardElement = createCard({ name: data.title, link: data.picture });
+  cardSection.addItem(newCardElement);
+  placePopup.close();
+});
+placePopup.setEventListeners();
+
+// --- Event Listeners de Botones ---
+
+document
+  .querySelector(".profile__edit-button")
+  .addEventListener("click", () => {
+    const { name, about } = userInfo.getUserInfo();
+    document.querySelector("#name-input").value = name;
+    document.querySelector("#description-input").value = about;
+    profilePopup.open();
+  });
+
+document.querySelector(".profile__add-button").addEventListener("click", () => {
+  placePopup.open();
+});
+
+// --- Validación ---
+const profileValidator = new FormValidator(
+  validationConfig,
+  document.querySelector("#edit-profile-form"),
+);
+const cardValidator = new FormValidator(
+  validationConfig,
+  document.querySelector("#edit-card-popup"),
+);
+profileValidator.setEventListeners();
+cardValidator.setEventListeners();
